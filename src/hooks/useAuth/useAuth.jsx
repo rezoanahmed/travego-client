3c48994fbe1b76b7a46d3414108786da13c21f81
Rezoan Ahmed <rezoan.cse.2017@gmail.com>
2023-11-06 02:12:33 +0600

    logout and update user functionality has been added

--- src/hooks/useAuth/useAuth.jsx
+++ src/hooks/useAuth/useAuth.jsx
@@ -1,32 +1,73 @@
+import { useEffect, useState } from "react";
 import { auth } from "../../firebase/firebase.config";
-import { GoogleAuthProvider, createUserWithEmailAndPassword, signInWithEmailAndPassword, signInWithPopup } from "firebase/auth";
+import { GoogleAuthProvider, createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut, updateProfile } from "firebase/auth";
 
 const useAuth = () => {
     
+    const [user,setUser] = useState([]);
+    const [loading, setLoading] = useState(true);
+
 
     // google login
     const googleLogin = async () =>{
+        setLoading(true);
         const provider = new GoogleAuthProvider();
         const result = await signInWithPopup(auth, provider);
         return result;
     }
 
     // register user
-    const register = async(email, password) =>{
+    const register = async(email, password, name, photo) =>{
+        setLoading(true)
         const result = await createUserWithEmailAndPassword(auth, email, password);
+        await updateUser(name, photo)
+        await logout();
         return result;
     }
 
     // login user
     const login = async(email,password) =>{
+        setLoading(true);
         const result = await signInWithEmailAndPassword(auth, email, password);
         return result;
     }
+
+    // logout user
+    const logout = async() =>{
+        const result = await signOut(auth);
+        return result;
+    }
+
+    // update user
+    const updateUser = async(name, photo)=>{
+        const result = await updateProfile(auth.currentUser, {
+            displayName: name,
+            photoURL: photo,
+        })
+        return result;
+    }
+
+
+    // manage users
+    useEffect(()=>{
+        const unsubscribe = onAuthStateChanged(auth, (currentUser)=>{
+            setUser(currentUser);
+            setLoading(false);
+        })
+        return ()=>{
+            unsubscribe();
+        }
+    },[])
 
     const loginSystems = {
         googleLogin,
         register,
         login,
+        logout,
+        user,
+        loading,
+        updateUser
+
 
     }
 
