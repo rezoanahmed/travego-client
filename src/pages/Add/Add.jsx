feb2a9b23f467dc4b9a834793305e1d2567f7021
Rezoan Ahmed <rezoan.cse.2017@gmail.com>
2023-11-06 19:05:36 +0600

    add section has been connected with mongodb

--- src/pages/Add/Add.jsx
+++ src/pages/Add/Add.jsx
@@ -1,17 +1,52 @@
-
+import axios from "axios"
+import Swal from "sweetalert2";
+import useAuth from "../../hooks/useAuth/useAuth";
 const Add = () => {
+    
+    const {user} = useAuth();
+    const handleAdd = e =>{
+        e.preventDefault();
+        const form = e.target;
+        const name = form.name.value;
+        const price = form.price.value;
+        const photo = form.photo.value;
+        const location = form.location.value;
+        const description = form.desc.value;
+
+        const service = {
+            name, price, photo, location, description, username: user.displayName, userPhoto: user.photoURL,
+        };
+        // console.log(service);
+        axios.post("http://localhost:3000/services", service)
+        .then(res=>{
+            if(res.data.insertedId){
+                // console.log(user.displayName);
+                // console.log(res.data.insertedId);
+                // console.log(res);
+                Swal.fire("","Service Added Successfully!!!", "success");
+
+            }
+        })
+        .catch(err=>{
+            if(err){
+                Swal.fire("", "Something Went Wrong!", "error");
+            }
+        })
+    }
+
+
     return (
         <div>
             <div className="text-center mt-10 font-bold text-4xl">
                 <h1>Add The Service You Want To Provide</h1>
             </div>
             <div>
-                <form className="grid md:grid-rows-3 md:grid-cols-2 gap-5 max-w-[500px] mx-auto py-10">
-                    <input className="border border-travego2 p-2 rounded-md shadow-md" type="text" placeholder="Service Name"/>
-                    <input className="border border-travego2 p-2 rounded-md shadow-md" type="text" placeholder="Price"/>
-                    <input className="border border-travego2 p-2 rounded-md shadow-md" type="text" placeholder="Photo URL"/>
-                    <input className="border border-travego2 p-2 rounded-md shadow-md" type="text" placeholder="Location"/>
-                    <input className="col-span-2 border border-travego2 p-2 rounded-md shadow-md" type="text" placeholder="Short Description"/>
+                <form onSubmit={handleAdd} className="grid md:grid-rows-3 md:grid-cols-2 gap-5 max-w-[500px] mx-auto py-10">
+                    <input name="name" className="border border-travego2 p-2 rounded-md shadow-md" type="text" placeholder="Service Name"/>
+                    <input name="price" className="border border-travego2 p-2 rounded-md shadow-md" type="text" placeholder="Price"/>
+                    <input name="photo" className="border border-travego2 p-2 rounded-md shadow-md" type="text" placeholder="Photo URL"/>
+                    <input name="location" className="border border-travego2 p-2 rounded-md shadow-md" type="text" placeholder="Location"/>
+                    <input name="desc" className="col-span-2 border border-travego2 p-2 rounded-md shadow-md" type="text" placeholder="Short Description"/>
                     <input type="submit" value="Add Service" className="col-span-2 bg-travego text-white hover:bg-travego2 p-2 rounded-md shadow-md" />
                 </form>
             </div>
