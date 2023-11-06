c228a18fbe9e1e7a1cf32ad0b096900705c3974f
Rezoan Ahmed <rezoan.cse.2017@gmail.com>
2023-11-06 12:27:43 +0600

    added hover effects in destination images

--- src/components/Destinations/Destination.jsx
+++ src/components/Destinations/Destination.jsx
@@ -4,7 +4,7 @@ const Destination = ({ destination }) => {
     const { name, city, country, image } = destination;
     return (
         <div className="">
-            <img src={image} alt="destinations" className="h-48 w-full" />
+            <img src={image} alt="destinations" className="h-48 w-full hover:scale-105 duration-300" />
                 <div className="-mt-20 w-48 p-2 absolute bg-black bg-opacity-50">
                     <p className="text-xl font-medium text-white">{name}</p>
                     <p className="text-white">{city},{country}</p>
