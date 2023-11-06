b62bbc29bfda8a284f668b4faffa8228d4324627
Rezoan Ahmed <rezoan.cse.2017@gmail.com>
2023-11-06 17:11:20 +0600

    created navbar as per instructions

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,23 +1,28 @@
 import { Link, NavLink } from "react-router-dom";
 import useAuth from "../../hooks/useAuth/useAuth";
-import {FiLogOut} from "react-icons/fi"
+import { FiLogOut } from "react-icons/fi"
 
 const Navbar = () => {
 
     const { user, logout } = useAuth();
-    const handleLogout = () =>{
+    const handleLogout = () => {
         logout()
-        
+
     }
     const links = <>
         <NavLink to='/' className="font-semibold text-gray-600 hover:text-travego sm:py-6 dark:text-blue-500">Home</NavLink>
         <NavLink to='/services' className="font-semibold text-gray-600 hover:text-travego sm:py-6 dark:text-blue-500">Services</NavLink>
-        <NavLink to='/' className="font-semibold text-gray-600 hover:text-travego sm:py-6 dark:text-blue-500">My Bookings</NavLink>
+
         <NavLink to='/about' className="font-semibold text-gray-600 hover:text-travego sm:py-6 dark:text-blue-500">About</NavLink>
         <NavLink to='/contact' className="font-semibold text-gray-600 hover:text-travego sm:py-6 dark:text-blue-500">Contact Us</NavLink>
-        <NavLink to='/add' className="font-semibold text-gray-600 hover:text-travego sm:py-6 dark:text-blue-500">Add Service</NavLink>
-        <NavLink to='/manage' className="font-semibold text-gray-600 hover:text-travego sm:py-6 dark:text-blue-500">Manage Services</NavLink>
+
     </>
+
+    const dashboard = <div className="flex flex-col">
+        <NavLink to='/' className="font-semibold text-gray-600 hover:text-travego p-2 dark:text-blue-500">My Bookings</NavLink>
+        <NavLink to='/add' className="font-semibold text-gray-600 hover:text-travego p-2 dark:text-blue-500">Add Service</NavLink>
+        <NavLink to='/manage' className="font-semibold text-gray-600 hover:text-travego p-2 dark:text-blue-500">Manage Services</NavLink>
+    </div>
     return (
         <div>
             <header className="flex flex-wrap sm:justify-start sm:flex-nowrap z-50 w-full bg-white border-b border-gray-200 text-sm py-3 sm:py-0 dark:bg-gray-800 dark:border-gray-700">
@@ -41,47 +46,22 @@ const Navbar = () => {
                         <div className="flex flex-col gap-y-4 gap-x-0 mt-5 sm:flex-row sm:items-center sm:justify-end sm:gap-y-0 sm:gap-x-7 sm:mt-0 sm:pl-7">
                             {links}
 
-                            {/* <div className="hs-dropdown [--strategy:static] sm:[--strategy:fixed] [--adaptive:none] sm:[--trigger:hover] sm:py-4">
-                                <button type="button" className="flex items-center w-full text-gray-500 hover:text-gray-400 font-medium dark:text-gray-400 dark:hover:text-gray-500 ">
-                                    Dropdown
-                                    <svg className="ml-2 w-2.5 h-2.5 text-gray-600" width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                        <path d="M2 5L8.16086 10.6869C8.35239 10.8637 8.64761 10.8637 8.83914 10.6869L15 5" stroke="currentColor" stroke-width="2" stroke-linecap="round"></path>
-                                    </svg>
-                                </button>
+                            {user? 
+                            <div className="hs-dropdown [--strategy:static] sm:[--strategy:fixed] [--adaptive:none] sm:[--trigger:hover] sm:py-4">
+                            <button type="button" className="flex items-center w-full text-gray-500 hover:text-gray-400 font-medium dark:text-gray-400 dark:hover:text-gray-500 ">
+                                Dashboard
+                                <svg className="ml-2 w-2.5 h-2.5 text-gray-600" width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M2 5L8.16086 10.6869C8.35239 10.8637 8.64761 10.8637 8.83914 10.6869L15 5" stroke="currentColor" stroke-width="2" stroke-linecap="round"></path>
+                                </svg>
+                            </button>
 
-                                <div className="hs-dropdown-menu transition-[opacity,margin] duration-[0.1ms] sm:duration-[150ms] hs-dropdown-open:opacity-100 opacity-0 sm:w-48 hidden z-10 bg-white sm:shadow-md rounded-lg p-2 dark:bg-gray-800 sm:dark:border dark:border-gray-700 dark:divide-gray-700 before:absolute top-full sm:border before:-top-5 before:left-0 before:w-full before:h-5">
-                                    <a className="flex items-center gap-x-3.5 py-2 px-3 rounded-md text-sm text-gray-800 hover:bg-gray-100 focus:ring-2 focus:ring-blue-500 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-gray-300" href="#">
-                                        About
-                                    </a>
-                                    <div className="hs-dropdown relative [--strategy:static] sm:[--strategy:absolute] [--adaptive:none] sm:[--trigger:hover]">
-                                        <button type="button" className="w-full flex justify-between w-full items-center text-sm text-gray-800 rounded-md py-2 px-3 hover:bg-gray-100 focus:ring-2 focus:ring-blue-500 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-gray-300">
-                                            Sub Menu
-                                            <svg className="sm:-rotate-90 ml-2 w-2.5 h-2.5 text-gray-600" width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                                <path d="M2 5L8.16086 10.6869C8.35239 10.8637 8.64761 10.8637 8.83914 10.6869L15 5" stroke="currentColor" stroke-width="2" stroke-linecap="round"></path>
-                                            </svg>
-                                        </button>
-
-                                        <div className="hs-dropdown-menu transition-[opacity,margin] duration-[0.1ms] sm:duration-[150ms] hs-dropdown-open:opacity-100 opacity-0 sm:w-48 hidden z-10 sm:mt-2 bg-white sm:shadow-md rounded-lg p-2 dark:bg-gray-800 sm:dark:border dark:border-gray-700 dark:divide-gray-700 before:absolute sm:border before:-right-5 before:top-0 before:h-full before:w-5 top-0 right-full !mx-[10px]">
-                                            <a className="flex items-center gap-x-3.5 py-2 px-3 rounded-md text-sm text-gray-800 hover:bg-gray-100 focus:ring-2 focus:ring-blue-500 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-gray-300" href="#">
-                                                About
-                                            </a>
-                                            <a className="flex items-center gap-x-3.5 py-2 px-3 rounded-md text-sm text-gray-800 hover:bg-gray-100 focus:ring-2 focus:ring-blue-500 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-gray-300" href="#">
-                                                Downloads
-                                            </a>
-                                            <a className="flex items-center gap-x-3.5 py-2 px-3 rounded-md text-sm text-gray-800 hover:bg-gray-100 focus:ring-2 focus:ring-blue-500 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-gray-300" href="#">
-                                                Team Account
-                                            </a>
-                                        </div>
-                                    </div>
-
-                                    <a className="flex items-center gap-x-3.5 py-2 px-3 rounded-md text-sm text-gray-800 hover:bg-gray-100 focus:ring-2 focus:ring-blue-500 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-gray-300" href="#">
-                                        Downloads
-                                    </a>
-                                    <a className="flex items-center gap-x-3.5 py-2 px-3 rounded-md text-sm text-gray-800 hover:bg-gray-100 focus:ring-2 focus:ring-blue-500 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-gray-300" href="#">
-                                        Team Account
-                                    </a>
-                                </div>
-                            </div> */}
+                            <div className="hs-dropdown-menu transition-[opacity,margin] duration-[0.1ms] sm:duration-[150ms] hs-dropdown-open:opacity-100 opacity-0 sm:w-48 hidden z-10 bg-white sm:shadow-md rounded-lg p-2 dark:bg-gray-800 sm:dark:border dark:border-gray-700 dark:divide-gray-700 before:absolute top-full sm:border before:-top-5 before:left-0 before:w-full before:h-5">
+                                {dashboard}
+                            </div>
+                        </div>
+                        :
+                        <></>
+                        }
 
                             <div>
                                 {
@@ -96,7 +76,7 @@ const Navbar = () => {
                                                     </div>
                                                 </div>
                                             </div>
-                                                <FiLogOut onClick={handleLogout} className="text-2xl" title="Log Out"></FiLogOut>
+                                            <FiLogOut onClick={handleLogout} className="text-2xl" title="Log Out"></FiLogOut>
 
                                         </div>
                                         :
