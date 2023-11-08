d6f076079a516c1987042c425b3dfdabfb0814b3
Rezoan Ahmed <rezoan.cse.2017@gmail.com>
2023-11-08 23:13:54 +0600

    created search box in hero section

--- src/components/Features/Features.jsx
+++ src/components/Features/Features.jsx
@@ -6,19 +6,19 @@ const Features = () => {
         <div className="mt-20">
             <div className="flex flex-col md:flex-row justify-center gap-10">
                 <div className="flex justify-center items-center flex-col gap-2">
-                    <BiSupport className="text-6xl text-blue-600"></BiSupport>
+                    <BiSupport className="text-6xl text-travego dark:text-blue-600"></BiSupport>
                     <h1 className="text-2xl font-bold">24/7 Customer Support</h1>
                 </div>
                 <div className="flex justify-center items-center flex-col gap-2">
-                    <BiSolidCustomize className="text-6xl text-blue-600"></BiSolidCustomize>
+                    <BiSolidCustomize className="text-6xl text-travego dark:text-blue-600"></BiSolidCustomize>
                     <h1 className="text-2xl font-bold">Personalized Services</h1>
                 </div>
                 <div className="flex justify-center items-center flex-col gap-2">
-                    <BiDollar className="text-6xl text-blue-600"></BiDollar>
+                    <BiDollar className="text-6xl text-travego dark:text-blue-600"></BiDollar>
                     <h1 className="text-2xl font-bold">Transparent Pricing</h1>
                 </div>
                 <div className="flex justify-center items-center flex-col gap-2">
-                    <VscFeedback className="text-6xl text-blue-600"></VscFeedback>
+                    <VscFeedback className="text-6xl text-travego dark:text-blue-600"></VscFeedback>
                     <h1 className="text-2xl font-bold">Feedback and Improvement</h1>
                 </div>
             </div>
