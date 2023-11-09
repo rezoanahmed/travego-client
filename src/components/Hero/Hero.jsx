9adb1a35eb76bad80ed26e97eebed6f228a77814
Rezoan Ahmed <rezoan.cse.2017@gmail.com>
2023-11-09 11:56:36 +0600

    added styles in search box

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,7 +1,24 @@
+import axios from "axios";
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import { TypeAnimation } from "react-type-animation";
 
 const Hero = () => {
+    // const [searchItem, setSearchItem] = useState([]);
+    // const search = (e) =>{
+    //     const searchValue = e.target.value;
+    //     // console.log(searchValue);
+    //     axios.get("https://travego-server.vercel.app/services")
+    //     .then(response=>{
+    //         // setServices(response.data);
+    //         // console.log(response.data);
+    //         setSearchItem(response.data)
+    //     })
+    //     console.log(searchItem);
+    //     const result = searchItem.filter(element => element.name.toLowerCase() == searchValue.toLowerCase())
+    //     console.log(result);
+        
+    // }
     return (
         <div>
             <div className="bg-[url('https://images.pexels.com/photos/2161467/pexels-photo-2161467.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1')] h-96 bg-cover bg-bottom">
@@ -29,7 +46,7 @@ const Hero = () => {
                     {/* <Link to='/login' className="bg-travego dark:dark:bg-[#1f2937] dark:text-[#949ea9] py-1 px-2 rounded-md hover:bg-travego2 duration-300 ease-in-out">Book Your Journey Now</Link> */}
                     <div className="flex flex-col md:flex-row">
                         <form>
-                            <input type="text" placeholder="Search Services" className="p-2 text-black bg-white dark:bg-black dark:text-white bg-opacity-40" />
+                            <input name="search" type="text" placeholder="Search Services" className="p-2  bg-black text-white bg-opacity-70" />
                             <button className="bg-travego dark:bg-blue-600 p-2">Search</button>
                         </form>
                     </div>
